03202c2d63448c28cf62b95f21c1f250030580c2
yuantian-cai <dlutcaiyuantian@hotmail.com>
2021-09-05 01:48:27 -0700

    edit

--- course-contents/course-1/exercise1.py
+++ course-contents/course-1/exercise1.py
@@ -4,15 +4,16 @@ def nth_digit(num: int, n: int) -> int:
         nth_digit(num, n) return the nth digit of num
         if n is too large or too small, return -1
     """
-    if (n < 0) or (10**n > num):
+    if (n != 0) and ((n < 0) or (10**n > num)):
         return -1
     else:
-        return num%(10**n)//(10**n)
+        return num%(10**(n+1))//(10**n)
 
 # question 2:  convert_to_upper:  Convert the strs in the given list to uppercase.
 def convert_to_upper(string_list: list[str]):
     """ Convert the strs in the given list to uppercase. """
-    pass
+    for i, value in enumerate(string_list):
+        string_list[i] = value.upper()
 
 
 # question 3
@@ -25,5 +26,18 @@ def get_x_strs(l: list[str]) -> list[str]:
 
 
 if __name__ == "__main__":
-    result = nth_digit(15, 1)
-    print(result, result == 1)
+    cases = [
+        [(233, 2), 2],
+        [(0, 0), 0],
+        [(100, 2), 1],
+        [(9987, -1), -1],
+        [(99, 2), -1]
+    ]
+    for case in cases:
+        result = nth_digit(case[0][0], case[0][1])
+        print(result, result == case[1]) 
+
+    str1 = ["abc", "ABC"]
+    convert_to_upper(str1)
+    print(str1, str1 == ["ABC", "ABC"])
+    
