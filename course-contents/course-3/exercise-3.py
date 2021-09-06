644be31e76efc25b0790ab366a5250de046db033
Hangyu Feng <hfeng@financeit.io>
2021-09-06 04:41:35 -0400

    add test case

--- course-contents/course-3/exercise-3.py
+++ course-contents/course-3/exercise-3.py
@@ -8,7 +8,7 @@ def find(l: list[int], n: int):
     if a == 0:
         return -1
     b = (a - 1) // 2
-    while b >= 0:        
+    while b >= 0:
         if l[b] == n:
             return b
         if l[b] > n:
@@ -16,7 +16,7 @@ def find(l: list[int], n: int):
         if l[b] < n:
             b = (b + a) // 2
         if l[0] > n or l[a - 1] < n:
-            return -1 
+            return -1
 
 
 if __name__ == "__main__":
@@ -24,6 +24,9 @@ if __name__ == "__main__":
         [[1, 2, 3, 4, 5], 1, 0],
         [[1, 2, 3, 4, 5], 5, 4],
         [[1, 2, 3, 4, 5], 6, -1],
+        [[], 0, -1],
+        [[1,2], -1, -1],
+        [[1,2,3,4,5,6,7,8,10], 9, -1]
     ]
     for l, n, result in cases:
         print(find(l, n), find(l, n) == result)
